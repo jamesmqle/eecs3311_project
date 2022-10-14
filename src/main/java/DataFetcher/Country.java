a9013d0365f726d9bf76d73a9801f64530097fc9
Zach <zacharyross1234@gmail.com>
2022-10-14 16:17:33 -0400

    Created filter classes Added return to fetchData function

---
+++ src/main/java/DataFetcher/Country.java
@@ -0,0 +1,22 @@
+package DataFetcher;
+
+public class Country extends Filter{
+
+    private String countryCode;
+
+    public Country () {
+        countryCode = "can";
+    }
+
+    public String getCountry() {
+        return countryCode;
+    }
+    public void setCountry(String country) {
+        countryCode = country;
+    }
+
+    public String toString() {
+        return "CountryCode: " + countryCode;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import DataFetcher.DataFetcherFacade;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
 public class Main {
 
@@ -6,8 +9,13 @@ public class Main {
 
         DataFetcherFacade.getInstance().setBaseURL("http://api.worldbank.org/v2");
 
-        DataFetcherFacade.getInstance().fetchData("SP.POP.TOTL");
-        //DataFetcherFacade.getInstance().fetchData("AG.LND.AGRI.ZS");
+        DataFetcherFacade.getInstance().dateRange.setRange("2010", "2020");
+        DataFetcherFacade.getInstance().country.setCountry("usa");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        for (JsonObject jsonObject: DataFetcherFacade.getInstance().fetchData("SP.POP.TOTL")) { // AG.LND.AGRI.ZS
+            System.out.println(gson.toJson(jsonObject));
+        }
 
     }
 
