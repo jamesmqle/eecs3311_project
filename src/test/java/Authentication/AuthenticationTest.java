94e92bd483ff7165368ef59b63116361e94b9a8c
jamesmqle <jamesmqle01@gmail.com>
2022-12-05 21:26:09 -0500

    create 3 tests for AuthenticationTest - TestLoginSuccess - TestLoginFail - TestUser

---
+++ src/test/java/Authentication/AuthenticationTest.java
@@ -0,0 +1,27 @@
+package Authentication;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthenticationTest {
+
+    @Test
+    public void TestLoginSuccess(){
+        assertTrue(AuthenticationFacade.getInstance().authenticate("zach", "1234"));
+        assertTrue(AuthenticationFacade.getInstance().authenticate("aiden", "abcd"));
+        assertTrue(AuthenticationFacade.getInstance().authenticate("james", "6666"));
+        assertTrue(AuthenticationFacade.getInstance().authenticate("vishal", "5656"));
+    }
+
+    @Test
+    public void TestLoginFail(){
+        assertFalse(AuthenticationFacade.getInstance().authenticate("username", "password"));
+        assertFalse(AuthenticationFacade.getInstance().authenticate("", ""));
+    }
+
+    @Test
+    public void TestUser() {
+        AuthenticationFacade.getInstance().currentUser.setUsername("userTest");
+        assertEquals("userTest", AuthenticationFacade.getInstance().currentUser.getUsername());
+    }
+}
